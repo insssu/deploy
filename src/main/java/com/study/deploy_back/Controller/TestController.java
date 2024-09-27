b02adfca3e16f357131f80b000ab5d44feb89b0e
insssu <xmrtn210845@naver.com>
2024-09-27 11:38:52 +0900

    init

---
+++ src/main/java/com/study/deploy_back/Controller/TestController.java
@@ -0,0 +1,21 @@
+package com.study.deploy_back.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${client.address}")
+    private String clientAddress;
+
+    @CrossOrigin
+    @GetMapping("/test")
+    public ResponseEntity<?> test() {
+        return ResponseEntity.ok(clientAddress + "테스트 !");
+    }
+}
